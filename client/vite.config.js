08a8fb796160e89a7fa5cfcd24f2319d38e8f4ab
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 02:53:30 -0300

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Configuración del proxy si es necesario
-    },
-    historyApiFallback: true,
-  },
 });
