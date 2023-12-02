6639743c43d0a6f110c7e034530b0d45108f8960
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 02:37:09 -0300

    a

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const path = require("path");
 const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
-const history = require("connect-history-api-fallback");
-
-// ... (otras importaciones)
-const { FRONTEND_URL } = process.env;
 
 const server = express();
 
@@ -20,20 +15,6 @@ server.use(
 
 server.use(history());
 
-// Sirve los archivos estáticos desde la carpeta "dist" de la carpeta "client"
-server.use(express.static(path.join(__dirname, "..", "client", "dist")));
-
 server.use(router);
-
-server.get("*", (req, res) => {
-  // Si no encuentra un archivo estático, sirve el archivo index.html
-  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = server;
