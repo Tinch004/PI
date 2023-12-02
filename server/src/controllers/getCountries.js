efcbe764a3521777e0de747d18b7efb111e5a26e
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 02:56:25 -0300

    a

--- server/src/controllers/getCountries.js
+++ server/src/controllers/getCountries.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { Country } = require("../db");
+const {Country} = require('../db');
 
 const getCountriesApi = async (req, res) => {
   try {
@@ -10,7 +10,7 @@ const getCountriesApi = async (req, res) => {
         const country = {
           id: pais.cca3,
           name: pais.name.common,
-          flagImage: pais.flags.png ? pais.flags.png : "Sin Bandera",
+          flagImage: pais.flags.png?pais.flags.png:'Sin Bandera',
           continents: pais.continents[0],
           capital: pais.capital ? pais.capital[0] : "No tiene capital",
           subregion: pais.subregion,
@@ -18,29 +18,37 @@ const getCountriesApi = async (req, res) => {
           population: pais.population,
         };
         Country.findOrCreate({
-          where: {
-            id: pais.cca3,
+                   
+          where:{
+
+              id: pais.cca3,
           },
 
           defaults: {
+              
             name: pais.name.common,
-            flagImage: pais.flags.png ? pais.flags.png : "Sin Bandera",
+            flagImage: pais.flags.png?pais.flags.png:'Sin Bandera',
             continents: pais.continents[0],
             capital: pais.capital ? pais.capital[0] : "No tiene capital",
             subregion: pais.subregion,
             area: pais.area ? pais.area : "no tiene area",
             population: pais.population,
-          },
-        });
+          }
+
+      }
+      )
 
         return country;
       })
     );
 
+
+
     return countries;
+
   } catch (error) {
-    throw Error("error al hacer peticion a la api");
+    throw Error('error al hacer peticion a la api')
   }
 };
 
-module.exports = { getCountriesApi };
+module.exports = {getCountriesApi};
