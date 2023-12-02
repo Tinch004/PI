ef5dcaadb0023021dffe54342648603a2cc14409
Martin Bruno <martinbruno30@gmail.com>
2023-12-01 21:28:12 -0300

    c

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    port: 5173,
+    open: true,
+  },
+
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
+
+  esbuild: {
+    jsxInject: `import React from 'react';`,
+  },
+
+  // Otras configuraciones específicas de Vite...
+});
