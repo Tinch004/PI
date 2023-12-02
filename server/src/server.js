766fa909942994fe710fed29ef18a565cd09ed69
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 02:38:51 -0300

    a

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
+const FRONTEND_URL = process.env;
 
 const server = express();
 
