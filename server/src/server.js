93fbd2b64b2cfd4e28f53e13ab25863c8297d698
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 01:06:23 -0300

    d

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
+const history = require("connect-history-api-fallback");
+
 const { FRONTEND_URL } = process.env;
 
 const server = express();
@@ -14,6 +17,23 @@ server.use(
   })
 );
 
+// Configura el middleware de reescritura de historial antes de tus rutas
+server.use(history());
+
 server.use(router);
+
+// Sirve los archivos estáticos desde la carpeta "client" que está un nivel atrás del "server"
+server.use(express.static(path.join(__dirname, "..", "client")));
+
+// Configura una ruta para todas las demás solicitudes, sirviendo el archivo index.html
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = server;
