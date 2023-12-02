e13449e19115de89bc7b3969d4b8b9f911ddb12a
Martin Bruno <martinbruno30@gmail.com>
2023-12-02 00:53:32 -0300

    d

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Configuración del proxy si es necesario
+    },
+    historyApiFallback: true,
+  },
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -16,19 +15,5 @@ server.use(
 );
 
 server.use(router);
-
-// Sirve los archivos estáticos desde la carpeta "client" que está un nivel atrás del "server"
-server.use(express.static(path.join(__dirname, "..", "client")));
-
-// Configura una ruta para todas las demás solicitudes, sirviendo el archivo index.html
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = server;
