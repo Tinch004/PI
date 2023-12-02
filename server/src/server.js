eebf30a91f96c5c787447193bd0d59102525d68b
Martin Bruno <martinbruno30@gmail.com>
2023-12-01 23:27:48 -0300

    d

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const router = require("./routes/index");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -11,10 +12,18 @@ server.use(cors());
 
 server.use(router);
 
-server.use(express.static(path.join(__dirname, "public")));
+// Sirve los archivos estáticos desde la carpeta raíz
+server.use(express.static(path.join(__dirname)));
 
+// Configura una ruta para todas las demás solicitudes, sirviendo el archivo index.html
 server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = server;
