3336f9d399802093c7730f07a368c0e95c1376e4
Martin Bruno <martinbruno30@gmail.com>
2023-12-01 21:04:01 -0300

    c

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 
 const Navbar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -19,24 +19,23 @@ const Navbar = ({ onSearch }) => {
     <div className={styles.navbar}>
       <div className={styles.navbar__container}>
         <div className={styles.navbar__input}>
-          <div></div><input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyPress={handleKeyPress}
-         
-          placeholder="Search..."
-        />
-        <button onClick={handleSearch} className={styles.navbar__button}>
-          🔍
-        </button></div>
-        
-      </div> 
-      
-       <NavLink to="/form" >
-          <button className={styles.actividad}>Create Activity</button>
-        </NavLink>
-     
+          <div></div>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyPress={handleKeyPress}
+            placeholder="Search..."
+          />
+          <button onClick={handleSearch} className={styles.navbar__button}>
+            🔍
+          </button>
+        </div>
+      </div>
+
+      <NavLink to="/form">
+        <button className={styles.actividad}>Create Activity</button>
+      </NavLink>
     </div>
   );
 };
